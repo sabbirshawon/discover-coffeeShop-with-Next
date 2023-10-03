ee77bb8be92cb45c66b708ecf8090ba3c2736c2d
Sabbir Ahmed <sabbir@yotech.ltd>
2023-10-03 11:21:22 +0600

    banner page updated

--- components/Banner.js
+++ components/Banner.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import styles from '../styles/banner.module.css';
 
-const Banner = (props) => {
-    return (
-        <div className={styles.container}>
-            <h1 className={styles.title}> <span className={styles.title1}>Coffee</span><span className={styles.title2}>Connoisseur</span></h1>
-             <p className={styles.subTitle}>Discover Your Local Coffee Shops!</p>
-             <div className={styles.buttonWrapper}>
-             <button className={styles.button} onClick={props.handleOnClick}>{props.bannerButtonText}</button>
-             </div>
-             
-        </div>
-    );
-};
+const Banner = ({ handleOnClick, bannerButtonText }) => (
+  <div className={styles.container}>
+    <h1 className={styles.title}>
+      <span className={styles.title1}>Coffee</span>
+      <span className={styles.title2}>Connoisseur</span>
+    </h1>
+    <p className={styles.subTitle}>Discover Your Local Coffee Shops!</p>
+    <div className={styles.buttonWrapper}>
+      <button className={styles.button} onClick={handleOnClick}>
+        {bannerButtonText}
+      </button>
+    </div>
+  </div>
+);
 
 export default Banner;
