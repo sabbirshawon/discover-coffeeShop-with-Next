dd630a35a13072f9baf314a8f864759c51e79f98
Sabbir Ahmed <sabbir@yotech.ltd>
2023-10-03 11:22:07 +0600

    store context updated

--- context/storeContext.js
+++ context/storeContext.js
@@ -1,32 +1,36 @@
-import { createContext, useReducer } from "react";
+import React, { createContext, useReducer } from "react";
 
-export const StoreContext = createContext();
-
+// Define action types as an object to avoid potential typos
 export const ACTION_TYPES = {
   SET_LAT_LONG: "SET_LAT_LONG",
   SET_COFFEE_STORES: "SET_COFFEE_STORES",
 };
 
+// Create the initial state as a constant
+const initialState = {
+  latLong: "",
+  coffeeStores: [],
+};
+
+// Create a separate reducer function for clarity
 const storeReducer = (state, action) => {
   switch (action.type) {
-    case ACTION_TYPES.SET_LAT_LONG: {
+    case ACTION_TYPES.SET_LAT_LONG:
       return { ...state, latLong: action.payload.latLong };
-    }
-    case ACTION_TYPES.SET_COFFEE_STORES: {
+    case ACTION_TYPES.SET_COFFEE_STORES:
       return { ...state, coffeeStores: action.payload.coffeeStores };
-    }
     default:
       throw new Error(`Unhandled action type: ${action.type}`);
   }
 };
 
-const StoreProvider = ({ children }) => {
-  const initialState = {
-    latLong: "",
-    coffeeStores: [],
-  };
+// Create the context
+export const StoreContext = createContext();
 
+// Create the provider component
+const StoreProvider = ({ children }) => {
   const [state, dispatch] = useReducer(storeReducer, initialState);
+
   return (
     <StoreContext.Provider value={{ state, dispatch }}>
       {children}
